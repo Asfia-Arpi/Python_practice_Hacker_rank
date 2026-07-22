6932c9e051928996d849bf633f85ea73cee49e79
[Asfia-Arpi] <[arpiasfia7@gmai.com]>
2026-07-22 13:55:14 +0600

    Solve: Division Problem

---
+++ 01-introduction/04_python_division_problem.py
@@ -0,0 +1,8 @@
+# HackerRank Problem: Arithmetic Operators
+# Topic: 01-introduction
+# File Name: 04_python_division_problem.py
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a // b)
+    print(a / b)
