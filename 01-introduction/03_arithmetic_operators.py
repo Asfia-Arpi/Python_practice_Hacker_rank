03a888735431b71884786bc608b0b52b5507138f
[Asfia-Arpi] <[arpiasfia7@gmai.com]>
2026-07-22 13:49:44 +0600

    Solve: Arithmetic Operator Problem

---
+++ 01-introduction/03_arithmetic_operators.py
@@ -0,0 +1,12 @@
+# HackerRank Problem: Arithmetic Operators
+# Topic: 01-introduction
+# File Name: 03_arithmetic_operators.py
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    sum = a+b
+    difference = a-b
+    product =a*b
+    print(sum)
+    print(difference)
+    print(product)
