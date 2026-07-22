902d2d98e3bc83082c58c23624b32d6327158299
[Asfia-Arpi] <[arpiasfia7@gmai.com]>
2026-07-23 02:26:56 +0600

    Solve: Leap year problem

---
+++ 01-introduction/06_leap_year_problem.py
@@ -0,0 +1,12 @@
+# HackerRank Problem: Arithmetic Operators
+# Topic: 01-introduction
+# File Name: 06_leap_year_problem.py
+
+def is_leap(year):
+    leap = False
+    if (year % 400 == 0) or(year % 100 != 0 and year % 4 == 0):
+        leap = True
+    return leap
+
+year = int(input())
+print(is_leap(year))
