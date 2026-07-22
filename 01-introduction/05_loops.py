b8ab079fc3da1916fc03f3d35cd6c463c039a197
[Asfia-Arpi] <[arpiasfia7@gmai.com]>
2026-07-22 14:48:27 +0600

    Solve: Python for loop problem

---
+++ 01-introduction/05_loops.py
@@ -0,0 +1,8 @@
+# HackerRank Problem: Arithmetic Operators
+# Topic: 01-introduction
+# File Name: 04_loops.py
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        print (i ** 2)
