630e8c0c9bba6bfe7a2e0dcde64cd3850e0f79ca
[Asfia-Arpi] <[arpiasfia7@gmai.com]>
2026-07-23 02:38:51 +0600

    Solve: Print function Problem

---
+++ 01-introduction/07_print_function.py
@@ -0,0 +1,9 @@
+# HackerRank Problem: Arithmetic Operators
+# Topic: 01-introduction
+# File Name: 06_leap_year_problem.py
+
+if __name__ == '__main__':
+    n = int(input())
+
+for i in range(1, n+1):
+    print(i, end=" ")
