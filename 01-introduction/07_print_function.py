7342163755c4eafb93669ff78ffb42e8959e82e2
[Asfia-Arpi] <[arpiasfia7@gmai.com]>
2026-07-23 03:22:45 +0600

    Solve: Print Function Problem

--- 01-introduction/07_print_function.py
+++ 01-introduction/07_print_function.py
@@ -1,9 +1,9 @@
 # HackerRank Problem: Arithmetic Operators
 # Topic: 01-introduction
-# File Name: 06_leap_year_problem.py
+# File Name: 07_print_function.py
 
 if __name__ == '__main__':
     n = int(input())
 
 for i in range(1, n+1):
-    print(i, end=" ")
+    print(i, end="")
